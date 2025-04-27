6ae51377da37354b24c7a754b53694a60bbc226f
LaanLee <lehailan110@gmail.com>
2025-04-27 12:32:58 +0700

    #update system

---
+++ Module2/Day8/main.js
@@ -0,0 +1,122 @@
+// console.log('Hello, World!');
+
+
+// let month = 4;
+
+// switch (month) {
+//     case 1:
+//     case 3:
+//     case 5:
+//     case 7:
+//     case 8:
+//     case 10:
+//     case 12:
+//         console.log('31 days');
+//         break;
+//     case 4:
+//     case 6:
+//     case 9:
+//     case 11:
+//         console.log('30 days');
+//         break;
+//     case 2:
+//         if (new Date().getFullYear() % 4 === 0) {
+//             console.log('29 days');
+//         }
+//         else {
+//             console.log('28 days');
+//         }
+//         break;
+//     default:
+//         console.log('Invalid month');
+//         break;
+// }
+
+
+// if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+//     console.log('31 days');
+// }
+// else if (month == 4 || month == 6 || month == 9 || month == 11) {
+//     console.log('30 days');
+// }
+// else if (month == 2) {
+//     console.log('29 days');
+// }
+// else {
+//     console.log('Invalid month');
+// }
+
+
+
+
+// let inp = prompt("Nhap chuoi: ");
+// let mes = inp.length >= 8 ? "Chuoi nay ok" : "Ngan qua, dai them ti nua";
+// console.log(mes);
+
+
+
+// let a = prompt("Nhap a: ");
+// if (a >= 18) {
+//     console.log("Du 18 thi quay tiep");
+// } else if (a >= 16) {
+//     console.log("Doi them it nam nua");
+// } else {
+//     console.log("Con qua na tre");
+// }
+
+
+
+// let sum = 0;
+// for (let i = 1; i <= 50; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+
+
+// let sum = 0;
+// for (let i = -10; i <= 50; i++) {
+//     if (i % 2 == 0) {
+//         sum += i;
+//     }
+// }
+
+// console.log(sum);
+
+
+
+// for (let i = -100; i <=100; i++) {
+//     if (i % 9 == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+// let i = 1;
+// while (i <= 200) {
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// let s = prompt("Nhap chuoi: ");
+// let l = prompt("Nhap so: ");
+// while (s.length < l) {
+//     s += 'a';
+// }
+
+// console.log(s);
+
+
+
+// let x = prompt("Nhap x: ");
+// let y = prompt("Nhap y: ");
+
+// for (x; x <= y; x++, y--) {
+//     console.log(x, y);
+
+//     if (x >= y) {
+//         break;
+//     }
+// }
